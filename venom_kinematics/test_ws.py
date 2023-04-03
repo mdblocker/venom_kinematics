79591b0ec2fd4907cb82bc4d587a3813531cd249
mdblocker <mdaltonblocker@gmail.com>
2023-04-03 16:37:01 -0600

    head test

--- venom_kinematics/test_ws.py
+++ venom_kinematics/test_ws.py
@@ -5,12 +5,16 @@ from kin_actions.kin_rotate_cw import *
 from kin_actions.kin_sym2rest import *
 from kin_actions.kin_rest2sym import *
 from kin_actions.kin_spin_up import *
+from kin_actions.kin_head_wakeup import *
 #--------------------------------------
 
-head_angle(0)
-time.sleep(1)
-head_angle(90)
-time.sleep(1)
-head_angle(180)
-time.sleep(1)
+speed = .2
+
+head_wakeup(speed)
+rotate_cw(speed)
+rotate_cw(speed)
+rotate_cw(speed)
+rotate_ccw(speed)
+rotate_ccw(speed)
+rotate_ccw(speed)
 
