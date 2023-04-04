f8637302202304ca718ae6099618d830c8dfe30e
mdblocker <mdaltonblocker@gmail.com>
2023-04-03 23:40:54 -0600

    head reconfig, reorg

---
+++ venom_kinematics/kin_actions/kin_headscan_y.py
@@ -0,0 +1,13 @@
+from kin_config import *
+from kin_nod import head_nod
+from kin_sup import head_sup
+import time
+#-----------------------
+
+def headscan_y(speed):
+
+    head_nod(speed)
+    head_sup(speed)
+    
+
+
---
+++ venom_kinematics/venom_kinematics_lib.py
@@ -0,0 +1,10 @@
+import time
+from kin_positions import *
+from kin_actions.kin_rotate_ccw import *
+from kin_actions.kin_rotate_cw import *
+from kin_actions.kin_sym2rest import *
+from kin_actions.kin_rest2sym import *
+from kin_actions.kin_spin_up import *
+from kin_actions.kin_headscan_y import *
+from kin_actions.kin_headscan_x import *
+#--------------------------------------
