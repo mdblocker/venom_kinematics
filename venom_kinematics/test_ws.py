957a892347f3b09b997fd21331daa1fc36d80447
mdblocker <mdaltonblocker@gmail.com>
2023-04-03 23:40:07 -0600

    head movement reorganize

--- venom_kinematics/test_ws.py
+++ venom_kinematics/test_ws.py
@@ -1,18 +1,12 @@
-import time
-from kin_positions import *
-from kin_actions.kin_rotate_ccw import *
-from kin_actions.kin_rotate_cw import *
-from kin_actions.kin_sym2rest import *
-from kin_actions.kin_rest2sym import *
-from kin_actions.kin_spin_up import *
-from kin_actions.kin_head_wakeup import *
-#--------------------------------------
+from venom_kinematics_lib import *
+#----------------------------------
 
 speed = .02
 
-head_wakeup(speed)
+headscan_y()
 
-head_angle_yes(35)
+head_angle_yes(50)
 time.sleep(.2)
 head_angle_no(90)
 time.sleep(.2)
+
