2f15aedf49b02c2485ed0972e9c961e5718cd35d
mdblocker <mdaltonblocker@gmail.com>
2023-05-21 22:48:08 -0600

    removing outdated scripts

--- venom_kinematics/venom_kinematics_lib.py
+++ venom_kinematics/venom_kinematics_lib.py
@@ -1,8 +1,6 @@
 import time
 from kin_config import *
 from kin_positions import *
-from kin_actions.kin_rotate_ccw import *
-from kin_actions.kin_rotate_cw import *
 from kin_actions.kin_headscan_y import *
 from kin_actions.kin_headscan_x import *
 from kin_actions.kin_wake_up import *
