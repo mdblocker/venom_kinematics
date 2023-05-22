ab810e64a00f21845eeb866a623b5360a9ac91f0
mdblocker <mdaltonblocker@gmail.com>
2023-05-21 23:41:06 -0600

    test up

--- venom_kinematics/kin_actions/kin_wake_up.py
+++ venom_kinematics/kin_actions/kin_wake_up.py
@@ -6,47 +6,13 @@ import time
 def wake_up(angle, interval, speed):
     rest()
     for x in range(0, angle, interval):
-        L1(90, (x)*2, 0)
-        L2(90, (x)*2, 0)
-        L3(90, (x)*2, 0)
-        L4(90, (x)*2, 0)
-        L5(90, (x)*2, 0)
-        L6(90, (x)*2, 0)
-        time.sleep(speed)
-
-    time.sleep(speed)
-    
-    for x in range(angle, 0, interval):
-        L3(90, x, 0)
-        time.sleep(speed)
-        L3(90, -x, 0)
-        time.sleep(speed)
-    for x in range(angle, 0, interval):
-        L5(90, x, 0)
-        time.sleep(speed)
-        L5(90, -x, 0)
-        time.sleep(speed)
-    for x in range(angle, 0, interval):
         L1(90, x, 0)
-        time.sleep(speed)
-        L1(90, -x, 0)
-        time.sleep(speed)
-    for x in range(angle, 0, interval):
-        L6(90, x, 0)
-        time.sleep(speed)
-        L6(90, -x, 0)
-        time.sleep(speed)
-    for x in range(angle, 0, interval):
         L2(90, x, 0)
-        time.sleep(speed)
-        L2(90, -x, 0)
-        time.sleep(speed)
-    for x in range(angle, 0, interval):
+        L3(90, x, 0)
         L4(90, x, 0)
+        L5(90, x, 0)
+        L6(90, x, 0)
+    
         time.sleep(speed)
-        L4(90, -x, 0)
-        time.sleep(speed)
-
-
 
 
