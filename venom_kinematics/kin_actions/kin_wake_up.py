60362f9ccc17de3717ac74d86ee20ed99395a907
mdblocker <mdaltonblocker@gmail.com>
2023-05-27 15:08:49 -0600

    wake_up

--- venom_kinematics/kin_actions/kin_wake_up.py
+++ venom_kinematics/kin_actions/kin_wake_up.py
@@ -3,16 +3,40 @@ from kin_positions import *
 import time
 #-----------------------
 
-def wake_up(angle, interval, speed):
+def wake_up(speed):
     rest()
-    for x in range(0, angle, interval):
-        L1(90, x, 0)
-        L2(90, x, 0)
-        L3(90, x, 0)
-        L4(90, x, 0)
-        L5(90, x, 0)
-        L6(90, x, 0)
-    
-        time.sleep(speed)
+    L1(90, 50, 0)
+    L2(90, 50, 0)
+    L3(90, 50, 0)
+    L4(90, 50, 0)
+    L5(90, 50, 0)
+    L6(90, 50, 0)
+    time.sleep(speed)
 
+    for x in range(50, 0, -2):
+        L3(90, x, 0)
+        time.sleep(speed)
+        L3(90, -x, 0)
+        time.sleep(speed)
+        L6(90, x, 0)
+        time.sleep(speed)
+        L6(90, -x, 0)
+        time.sleep(speed)
+        L2(90, x, 0)
+        time.sleep(speed)
+        L2(90, -x, 0)
+        time.sleep(speed)
+        L4(90, x, 0)
+        time.sleep(speed)
+        L4(90, -x, 0)
+        time.sleep(speed)
+        L1(90, x, 0)
+        time.sleep(speed)
+        L1(90, -x, 0)
+        time.sleep(speed)
+        L5(90, x, 0)
+        time.sleep(speed)
+        L5(90, -x, 0)
+        time.sleep(speed)
+      
 
