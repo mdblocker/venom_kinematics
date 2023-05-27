28b1092df29290c17650bf8a1886259ee7ecfb08
mdblocker <mdaltonblocker@gmail.com>
2023-05-27 16:23:49 -0600

    wake up dev

--- venom_kinematics/test_ws.py
+++ venom_kinematics/test_ws.py
@@ -5,6 +5,10 @@ speed = .02
 
 wake_up(speed)
 
-for x in range(50, 5, 1):
+for x in range(50, 5, -2):
     L1(90, x, 0)
     time.sleep(speed) 
+for x in range(5, 50, 2):
+    L1(90, x, 0)
+    time.sleep(speed)
+    
