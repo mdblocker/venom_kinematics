eb82d3c742b94aa9ef225986b4624267737ade38
mdblocker <mdaltonblocker@gmail.com>
2023-06-15 14:59:01 -0600

    new start

--- venom_kinematics/test_ws.py
+++ venom_kinematics/test_ws.py
@@ -8,10 +8,57 @@ wake_up(speed)
 headscan_x(speed)
 headscan_y(speed)
 
+#1
 for x in range(50, 0, -1):
     L1(90, x, 0)
     time.sleep(.02)
-
-for y in range(0, 50, 1):
+time.sleep(speed)
+for y in range(0, 30, 1):
     L1(90, y, 0)
     time.sleep(.02)
+time.sleep(speed)
+#6
+for x in range(50, 0, -1):
+    L6(90, x, 0)
+    time.sleep(.02)
+time.sleep(speed)
+for y in range(0, 30, 1):
+    L6(90, y, 0)
+    time.sleep(.02)
+time.sleep(speed)
+#3
+for x in range(50, 0, -1):
+    L3(90, x, 0)
+    time.sleep(.02)
+time.sleep(speed)
+for y in range(0, 30, 1):
+    L3(90, y, 0)
+    time.sleep(.02)
+time.sleep(speed)
+#4
+for x in range(50, 0, -1):
+    L4(90, x, 0)
+    time.sleep(.02)
+time.sleep(speed)
+for y in range(0, 30, 1):
+    L4(90, y, 0)
+    time.sleep(.02)
+time.sleep(speed)
+#2
+for x in range(50, 0, -1):
+    L2(90, x, 0)
+    time.sleep(.02)
+time.sleep(speed)
+for y in range(0, 30, 1):
+    L2(90, y, 0)
+    time.sleep(.02)
+time.sleep(speed)
+#5
+for x in range(50, 0, -1):
+    L5(90, x, 0)
+    time.sleep(.02)
+time.sleep(speed)
+for y in range(0, 30, 1):
+    L5(90, y, 0)
+    time.sleep(.02)
+time.sleep(speed)
