a7a6d02d0b392a7548c9dc6166cdf922fbc924f9
mdblocker <mdaltonblocker@gmail.com>
2023-06-15 15:55:54 -0600

    test

--- venom_kinematics/test_ws.py
+++ venom_kinematics/test_ws.py
@@ -9,20 +9,20 @@ headscan_x(speed)
 headscan_y(speed)
 
 #3
-for x in range(110, 0, -4):
+for x in range(100, 0, -4):
     L3(90, x, 30)
     time.sleep(.02)
 time.sleep(speed)
-for y in range(0, 110, 4):
+for y in range(0, 100, 4):
     L3(90, y, 30)
     time.sleep(.02)
 time.sleep(speed)
 #4
-for x in range(110, 0, -4):
+for x in range(100, 0, -4):
     L4(90, x, 30)
     time.sleep(.02)
 time.sleep(speed)
-for y in range(0, 110, 4):
+for y in range(0, 100, 4):
     L4(90, y, 30)
     time.sleep(.02)
 time.sleep(speed)
